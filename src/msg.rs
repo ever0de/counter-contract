f3b55bfca06b33e8a800e34df4abda3ac8ac1fb3
Jiseok CHOI <jiseok.dev@gmail.com>
2022-06-16 21:36:49 +0900

    Add `rename_all` container attr

--- src/msg.rs
+++ src/msg.rs
@@ -2,6 +2,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub struct InstantiateMsg {
     pub count: i32,
 }
@@ -22,6 +23,7 @@ pub enum QueryMsg {
 
 //// We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
 pub struct CountResponse {
     pub count: i32,
 }
